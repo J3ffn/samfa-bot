5fbab17889e559cbeeec641257dfac2b537c6984
j3ffn <jefferson.mangueira12@gmail.com>
2024-09-22 23:13:16 -0300

    BOT

---
+++ loader.js
@@ -0,0 +1,80 @@
+const { readdirSync } = require("fs");
+const { Collection } = require("discord.js");
+const { useMainPlayer } = require("discord-player");
+client.commands = new Collection();
+const commandsArray = [];
+const player = useMainPlayer();
+
+const { Translate, GetTranslationModule } = require("./process_tools");
+
+const discordEvents = readdirSync("./events/Discord/").filter((file) =>
+  file.endsWith(".js")
+);
+const playerEvents = readdirSync("./events/Player/").filter((file) =>
+  file.endsWith(".js")
+);
+
+GetTranslationModule().then(() => {
+  console.log("| Translation Module Loaded |");
+
+  for (const file of discordEvents) {
+    const DiscordEvent = require(`./events/Discord/${file}`);
+    const txtEvent = `< -> > [Loaded Discord Event] <${file.split(".")[0]}>`;
+    parseLog(txtEvent);
+    client.on(file.split(".")[0], DiscordEvent.bind(null, client));
+    delete require.cache[require.resolve(`./events/Discord/${file}`)];
+  }
+
+  for (const file of playerEvents) {
+    const PlayerEvent = require(`./events/Player/${file}`);
+    const txtEvent = `< -> > [Loaded Player Event] <${file.split(".")[0]}>`;
+    parseLog(txtEvent);
+    player.events.on(file.split(".")[0], PlayerEvent.bind(null));
+    delete require.cache[require.resolve(`./events/Player/${file}`)];
+  }
+
+  readdirSync("./commands/").forEach((dirs) => {
+    const commands = readdirSync(`./commands/${dirs}`).filter((files) =>
+      files.endsWith(".js")
+    );
+
+    for (const file of commands) {
+      const command = require(`./commands/${dirs}/${file}`);
+      if (command.name && command.description) {
+        commandsArray.push(command);
+        const txtEvent = `< -> > [Loaded Command] <${command.name.toLowerCase()}>`;
+        parseLog(txtEvent);
+        client.commands.set(command.name.toLowerCase(), command);
+        delete require.cache[require.resolve(`./commands/${dirs}/${file}`)];
+      } else {
+        const txtEvent = `< -> > [Failed Command] <${command.name.toLowerCase()}>`;
+        parseLog(txtEvent);
+      }
+    }
+  });
+
+  client.on("ready", async (client) => {
+    console.log(commandsArray);
+    try {
+      if (client.config.app.global) {
+        await client.application.commands.set(commandsArray);
+        console.log("Comandos registrados globalmente.");
+      } else {
+        const guild = client.guilds.cache.get(client.config.app.guild);
+        if (!guild) {
+          console.error("Guilda não encontrada!");
+          return;
+        }
+        await guild.commands.set(commandsArray);
+        console.log(`Comandos registrados na guilda: ${guild.name}`);
+      }
+    } catch (error) {
+      console.error("Erro ao registrar os comandos:", error);
+    }
+    
+  });
+
+  async function parseLog(txtEvent) {
+    console.log(await Translate(txtEvent, null));
+  }
+});
